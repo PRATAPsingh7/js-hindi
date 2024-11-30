79522800bb3d65703e0c54422d0541a5d5fd5796
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-11-30 08:35:49 +0000

    done the conversion

---
+++ 01_basicsjs/03_convertionOperations.js
@@ -0,0 +1,37 @@
+let score1 = "33"
+let score2= null
+let score3 = true
+let score4 = "55ab"
+let loged = 1
+
+
+console.log(  score1);
+console.log(  score2);
+console.log(  score3);
+console.log(  score4);
+
+
+
+
+
+
+console.log( typeof score1);
+console.log( typeof score2);
+console.log( typeof score3);
+console.log( typeof score4);
+
+
+let value1 = Number(score1)
+let value2 = Number(score2)
+let value3 = Number(score3)
+let value4 = Number(score4)
+let value5 = Boolean(loged)
+
+
+console.log(value1);
+console.log(value2);
+console.log(value3); 
+console.log(value4); 
+console.log(value5); 
+
+
