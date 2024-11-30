254dcf6f2ffbc28a58b4987d8f5f1613750cdf82
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-11-30 06:06:36 +0000

    new changw

---
+++ 01_basicsjs/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// alert is not used in node js, onply in browser//
+
+console.log(3+3);
+console.log("pratap");
+
+
+let name = "pratap"
+let age = 20
+let isloggedIn = false
+
+
+
+//number =>2 power 53
+// bigint for large nunmbers
+// string ""
+//boolean true/false
+// null =>stand alone value 
+// undefined => not assigned
+// symbol => uniqe 
+
+
+// object
+
+
+console.log(typeof null  );
+
--- 02_dataTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// alert is not used in node js, onply in browser//
-
-console.log(3+3);
-console.log("pratap");
-
