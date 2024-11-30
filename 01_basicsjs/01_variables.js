b83afcf0a18701b0df1669fc15e7f8e633990760
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-11-30 04:19:33 +0000

    learnig 02

---
+++ 01_basicsjs/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "itsps87@gmail.com"
+var accountPassword = "12354"
+accountCity = "Delhi"
+let accountState ;
+
+// accountId = 2
+accountEmail = "hfvsjbhks@gmail.com"
+accountPassword = "4321"
+accountCity = "mumbai"
+
+
+/*do not use var 
+because of issue in fuunctional issue */
+
+console.log(accountId);
+
+console.table([accountEmail, accountId,accountPassword,accountCity, accountState])
---
+++ 02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// alert is not used in node js, onply in browser//
+
+console.log(3+3);
+console.log("pratap");
+
