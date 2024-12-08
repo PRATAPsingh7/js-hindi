840c4f2628171322637f9a1af2ff4b89a8248cf5
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-08 12:45:17 +0000

    learned sting

---
+++ 01_basicsjs/05_stings.js
@@ -0,0 +1,35 @@
+const GameName = new String('gand');
+
+console.log(GameName);
+console.log(GameName[2]);
+console.log(GameName.toUpperCase());
+console.log(GameName.toLocaleUpperCase());
+console.log(GameName.charAt(2));
+
+// substring,slice
+
+
+
+// trim
+const Ganduu = "        Hitesh   "
+console.log(Ganduu);
+console.log(Ganduu.trim());
+
+// replace
+
+const url = "Bandar kya jane adrak ka swad";
+console.log(url);
+
+console.log(url.replace('adrak','gand'));
+
+
+// present or not
+
+console.log(url.includes( 'kya'));
+console.log(url.includes( 'pratap'));
+
+// split
+
+const Name = new String('abra-ka-dabra');
+console.log(Name.split( ' - ' ));
+
