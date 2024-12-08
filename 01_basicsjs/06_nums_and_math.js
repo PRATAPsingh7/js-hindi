9246453a052d2cfc3b7bd8620debac7874c444a7
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-08 15:55:09 +0000

    csc

--- 01_basicsjs/06_nums_and_math.js
+++ 01_basicsjs/06_nums_and_math.js
@@ -1,7 +1,54 @@
-const score = 400
-console.log(score);
+// const score = 400
+// console.log(score);
 
-const Balance = new Number(100);
-console.log(Balance);
+// const Balance = new Number(100);
+// console.log(Balance);
+// console.log(Balance.toString().length);
+// console.log(Balance.toString().repeat(4));
+
+
+// // precision
+
+// const value = 123.589
+// console.log(value.toPrecision(2));
+// console.log(value.toPrecision(3));
+// console.log(value.toPrecision(4));
+// console.log(value.toPrecision(5));
+
+// // number Representation
+
+// const networth = 10000000
+// console.log(networth.toLocaleString());
+// console.log(networth.toLocaleString('en-IN'));
+
+
+// ===========================MATH==================================
+
+
+console.log(Math);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
