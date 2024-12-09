c7f0bb66723aa37b66b83491aa0e38b074005349
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-09 02:10:04 +0000

    tdyjm

---
+++ 01_basicsjs/07_dates.js
@@ -0,0 +1,46 @@
+// Dates
+
+
+
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate); 
+
+// create particular date
+
+ const LMD = new Date(2003,2,15);
+
+//  console.log(LMD);
+
+//  console.log(LMD.toDateString());
+
+
+//  time stamps
+
+// let Mts = Date.now()
+
+// console.log(Mts);
+
+
+let
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
