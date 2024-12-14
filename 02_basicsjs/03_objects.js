9529545adb4b076bf016f46a0a9f9e893c08fe16
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-14 08:01:46 +0000

    symbol

---
+++ 02_basicsjs/03_objects.js
@@ -0,0 +1,21 @@
+// the ways for object declarations 
+
+// (litrel,constructor , )
+// singletone
+
+//object literals 
+
+const jsUser = {
+    name : "pratap",
+    age  : "20",
+    location : "vijayawada",
+    email: "pratapsingh.connect@gmail.com",
+    isLogedIn : "fallse",
+    lastLogin : ["monday","saturday"]
+
+
+}
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+
+
