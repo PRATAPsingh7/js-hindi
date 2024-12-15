768c4ee0956abaaef9fa0ccd39f599243f33be0b
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-15 08:46:46 +0000

    tjdry

--- 02_basicsjs/03_objects.js
+++ 02_basicsjs/03_objects.js
@@ -5,8 +5,12 @@
 
 //object literals 
 
+const Newsym = Symbol("key1");
+
 const jsUser = {
     name : "pratap",
+    full_name : "swa pratap" ,
+    [Newsym] : "key1",
     age  : "20",
     location : "vijayawada",
     email: "pratapsingh.connect@gmail.com",
@@ -15,7 +19,35 @@ const jsUser = {
 
 
 }
-console.log(jsUser.email);
-console.log(jsUser["email"]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+
+// console.log(jsUser["full_name"]);
+
+// console.log(jsUser[Newsym]);
+
+// jsUser.email = "pratapsingh.connect65@gmail.com"
+// Object.freeze(jsUser)
+// jsUser.email = "ijvobnd@gmail.com"
+// console.log(jsUser["email"]);
+// console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("hello user ");
+
+}
+jsUser.greeting2 = function(){
+    console.log(`hello user ${this.name} `);
+    
+    
+}
 
 
+console.log(jsUser.greeting());
+console.log(jsUser.greeting2());
+
+console.log(`hwllo ${this.name}`); //this is 
+
+console.log(`hello user ${jsUser.name} `); // area 
+
+ 
