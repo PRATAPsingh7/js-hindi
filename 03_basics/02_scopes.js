eef02a421bf37d2e8cfd8b6919d74af19ac4af14
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-18 11:47:21 +0000

    jk

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,19 +2,33 @@
 
 
 //{} this is scope
-let a = 1000
-if (true) {
-    let a = 10
-    const b = 20
-    // var c = 30 it is not requred
-    console.log("inner : ",a);
+// let a = 1000
+// function (true) {
+//     let a = 10
+//     const b = 20
+//     // var c = 30 it is not requred
+//     console.log("inner : ",a);
     
-    
+
+// }
+
+
+// console.log(a);
+
+function one() {
+    const username = "pratap "
+
+        function two() {
+            const website = "youtube"
+            console.log(username);
+            
+        }
+// console.log(website);
+
+two()
+
+
 
 }
-
-
-
-
-console.log(a);
-
+ 
+one()
