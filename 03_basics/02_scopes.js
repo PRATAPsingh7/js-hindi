737498de41823bfda58b9fbbaebc1c4f6d066f2c
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-18 10:27:08 +0000

    nm

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,20 @@
+// let var const 
+
+
+//{} this is scope
+let a = 1000
+if (true) {
+    let a = 10
+    const b = 20
+    // var c = 30 it is not requred
+    console.log("inner : ",a);
+    
+    
+
+}
+
+
+
+
+console.log(a);
+
