660f2d8191dc8342abadda6bfef15fc8521a69a1
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-18 08:12:54 +0000

    rgu

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,75 @@
 
-function myName (){
-    console.log("p");
-    console.log("r");
-console.log("a");
-console.log("t");
-console.log("a");
-console.log("p");
+// function myName (){
+//     console.log("p");
+//     console.log("r");
+// console.log("a");
+// console.log("t");
+// console.log("a");
+// console.log("p");
+
+// }
+
+
+// function addNumbers (number1,number2){
+//     number1+number2
+//     console.log(number1+number2);
+    
+
+// }
+
+// addNumbers(8,9)
+// addNumbers(8,"a")
+// addNumbers(8,"3")
+// addNumbers(8,null)
+
+// const result = addNumbers(1,5)
+// console.log(result);
+
+
+
+function addNumbers (number1,number2){
+   let result = number1+number2
+   console.log(number1+number2);
+   return result
+   console.log(result);
+   
 
 }
-myName()
+const result = addNumbers(1,5)
+console.log(result);
+
+
+
+
+function addkarna (a,b){
+
+    return a + b
+}
+
+
+console.log(addkarna(1,1));
+
+
+//  ~~~~~~~~~~~~~~
+
+function loginUserMessage(username) {
+    if( username === undefined){
+        console.log("please enter a name ");
+        return
+    }
+    return `${username} just logged in`
+    
+}
+console.log (loginUserMessage());
+
+
+
+
+
+function kaiseHO(person) {
+
+    return `${person} acha hai`
+    
+}
+
+console.log(kaiseHO("pratap"));
