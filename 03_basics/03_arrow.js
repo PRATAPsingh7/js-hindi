a545a312f1c4956812a1edec20bc048a81a5227e
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2024-12-19 16:40:38 +0000

    nj

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,27 @@
+// function chai() {
+//     let username = "pratap"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+// ~~~~~~~~~~~~~~~~~~~~Arrow function~~~~~~~~~~~~~~~~~~~~~~~~
+
+const chai1 =  () => {
+    let username = "pratap"
+    console.log(this);
+    
+
+
+}
+chai1()
+
+// const chai =  function() {
+//     let username = "pratap"
+//     console.log(this);
+    
+
+
+// }
+// chai()
