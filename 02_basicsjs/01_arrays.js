b0947a1eba2243729cf4744f61492944c5a8b521
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2025-07-14 16:49:57 +0000

    hh

--- 02_basicsjs/01_arrays.js
+++ 02_basicsjs/01_arrays.js
@@ -1,9 +1,48 @@
-// arrays
+// // arrays
 
-const myarr = [0,1,9,3,4,5,6]
-const ma2 = ["usiw", "qjnekd", "jckd","djnu"]
+// const myarr = [0,1,9,3,4,5,6]
+// const ma2 = ["usiw", "qjnekd", "jckd","djnu"]
+// // ('') or ("") both can create array
+
+// console.log(ma2);
+// console.log(ma2[5]);
+// console.log(ma2[4]);
+// console.log(ma2[1]);
+// console.log(ma2[0]);
+
+// console.log(myarr[2]);
 
 
-console.log(ma2[2]);
+const newarr = new Array ('hello','hello')
+console.log(newarr);
+ 
+// //push usage for extra methods to add
 
-console.log(myarr[2]);
+// newarr.push(2)
+// console.log(newarr)
+
+// // to remove last value use pop
+// newarr.pop()
+// console.log(newarr)
+// newarr.pop(1)
+// console.log(newarr)
+
+// // unshit is used to forward vales
+// newarr.unshift("naya" ,"k")
+// console.log(newarr)
+
+// // shift is also used to remove forwad vales
+// newarr.shift()
+// console.log(newarr)
+
+
+
+//cAN BE USED TO CONVERT Array INTO String
+
+const nayaarr = newarr.join()
+
+
+//slice,splice
+
+
+
