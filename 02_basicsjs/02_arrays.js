a0e7d44db0246ab3e899ccd8bbbe94cea7547ed3
PRATAPsingh7 <159470450+PRATAPsingh7@users.noreply.github.com>
2025-07-22 16:03:25 +0000

    Uncomment hero arrays and related operations for demonstration; fix array flattening syntax

--- 02_basicsjs/02_arrays.js
+++ 02_basicsjs/02_arrays.js
@@ -1,18 +1,35 @@
-const marvel_heros = ["thor","Ironman","spiderman"]
-const dc_heros = ["superman","blue bettel","flash"]
+// const marvel_heros = ["thor","Ironman","spiderman"]
+// const dc_heros = ["superman","blue bettel","flash"]
 
 // const allheros = marvel_heros.concat(dc_heros)
 
 // console.log(allheros);
 
-// spread operator(for multiple values)
+// // spread operator(for multiple values)
 
-const all_new_heros = [...marvel_heros,...dc_heros]
+// const all_new_heros = [...marvel_heros,...dc_heros]
 
 // console.log(all_new_heros);
 
-const another_array = [1,2,3,[4,5,6],7,[6,7[4,5]]]
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+console.log(another_array);
+const real_AA = another_array.flat(Infinity)
+console.log(real_AA);
 
-const real_another_array = another_array.flat(IIInfinity)
 
-console.log(real_another_array);
+
+// const real_another_array = another_array.flat(IIInfinity)
+
+// console.log(real_another_array);
+
+// to convert into array 
+
+console.log(Array.from("HELLO"));
+// console.log(Array.from(HELLO)
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2, score3));
+console.log(Array.of("HELLO"));
+
